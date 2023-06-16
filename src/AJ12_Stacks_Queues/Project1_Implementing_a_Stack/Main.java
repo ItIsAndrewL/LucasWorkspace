13a7e3390b6b301e0bab349ef9c2d47110c784ed
ItIsAndrewL <andrew.lockard15@gmail.com>
2023-06-16 17:47:27 -0400

    Add AJ12 Project 1

---
+++ src/AJ12_Stacks_Queues/Project1_Implementing_a_Stack/Main.java
@@ -0,0 +1,7 @@
+package AJ12_Stacks_Queues.Project1_Implementing_a_Stack;
+
+class Main {
+    public static void main(String[] args) {
+        //TODO test the stack here!
+    }
+}
---
+++ src/AJ12_Stacks_Queues/Project1_Implementing_a_Stack/UnderflowException.java
@@ -0,0 +1,4 @@
+package AJ12_Stacks_Queues.Project1_Implementing_a_Stack;
+
+public class UnderflowException extends RuntimeException{
+}
