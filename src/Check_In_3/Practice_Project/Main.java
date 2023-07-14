e2ffd6699ce8e28e585a5244b18eea9b6818e87f
ItIsAndrewL <andrew.lockard15@gmail.com>
2023-07-14 18:28:23 -0400

    Add Practice Project

---
+++ src/Check_In_3/Practice_Project/Main.java
@@ -0,0 +1,19 @@
+package Check_In_3.Practice_Project;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Queue<String> queue = new LinkedList<>();
+        String[] names = {"Kyle", "Johnny", "Carlos", "Brian", "Asia", "Chloe", "Melissa", "Finn", "Brooklyn", "Kesha", "Gavin", "Isabella", "Andre", "Matias", "Ebony", "Devin", "James", "Shawn"};
+        queue.addAll(List.of(names));
+        // TODO: Call reverse, print out, call alphabetize, print out again
+    }
+
+    private static void reverse(Queue<String> queue){
+        // TODO
+    }
+    private static void alphabetize(Queue<String> queue){
+        // TODO
+    }
+}
---
+++ src/Check_In_3/Practice_Project/instructions.md
@@ -0,0 +1,5 @@
+# Practice Project Instructions
+
+You are given a queue that is filled with a variety of names. Use a stack to reverse the contents of the queue, and use a priority queue to sort them alphabetically. Print the queue after each transformation.
+
+[Example Video](https://static.junilearning.com/java_level_3/aj_check-in_3.mp4)
