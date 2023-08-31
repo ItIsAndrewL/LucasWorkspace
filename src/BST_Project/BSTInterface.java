621580309219b1d540ce6cdbf88a66ff2365ce7d
ItIsAndrewL <andrew.lockard15@gmail.com>
2023-08-31 17:11:02 -0400

    Try 2

---
+++ src/BST_Project/BSTInterface.java
@@ -0,0 +1,8 @@
+package BST_Project;
+
+public interface BSTInterface {
+    public int size();
+    public boolean isEmpty();
+    public void insert(int value);
+    public boolean has(int value);
+}
---
+++ src/BST_Project/Main.java
@@ -0,0 +1,23 @@
+package BST_Project;
+
+public class Main {
+    public static void main(String[] args) {
+        BST metal = new BST();
+        metal.insert(5);
+        metal.insert(7);
+        metal.insert(6);
+        metal.insert(2);
+        metal.insert(12);
+        metal.insert(1);
+        metal.insert(4);
+        //14261275
+        //52137612
+        //12456712
+        //System.out.println(metal.postOrder());
+        //System.out.println(metal.preOrder());
+        //System.out.println(metal.inOrder());
+        metal.clear();
+        System.out.println(metal.inOrder());
+        System.out.println("hello!");
+    }
+}
